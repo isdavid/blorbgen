cd61c89566d4e8602d06c345259961a9325b20d9
David Thompson <daviddt17@gmail.com>
2020-08-25 22:37:00 -0400

    Added Material-UI Core, Icons, and Roboto Font

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'fontsource-roboto';
 import './index.css';
 import App from './components/app/App';
 import * as serviceWorker from './serviceWorker';
